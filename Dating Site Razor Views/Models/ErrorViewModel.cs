3764964dad711be82f4de479d428ae5013a9c4e2
Johnny Tran <jojox210@gmail.com>
2024-04-03 19:33:29 -0400

    Add project files.

---
+++ Dating Site Razor Views/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dating_Site_Razor_Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
