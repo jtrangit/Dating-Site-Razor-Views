287af21b72df281f4c334884c41e30ff86f28071
Johnny Tran <jojox210@gmail.com>
2024-04-04 20:41:41 -0400

    Working on converting login page to razor.

---
+++ Dating Site Razor Views/Models/LoginValidation.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Dating_Site_Razor_Views.Models
+{
+    public class LoginValidation
+    {
+        [Required(ErrorMessage = "Username Required")]
+        public string Username {  get; set; }
+
+        public string Password { get; set; }
+    }
+}
