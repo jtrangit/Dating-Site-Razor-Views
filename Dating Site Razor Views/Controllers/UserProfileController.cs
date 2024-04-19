261154698ff2f799c22dd1e4bc701e0d6243b805
Johnny Tran <jojox210@gmail.com>
2024-04-19 19:59:03 -0400

    user profile model binding wip

--- Dating Site Razor Views/Controllers/UserProfileController.cs
+++ Dating Site Razor Views/Controllers/UserProfileController.cs
@@ -1,12 +1,90 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dating_Site_Razor_Views.Pages;
+using DatingSiteLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Dating_Site_Razor_Views.Controllers
 {
     public class UserProfileController : Controller
     {
-        public IActionResult Index()
+        public IActionResult UserProfile()
         {
-            return View();
+            int accID = Convert.ToInt32(HttpContext.Session.GetString("accountID"));
+
+            Dating dating = new Dating();
+            DataSet userInfo = new DataSet();
+
+            userInfo = dating.getProfileInfo(accID);
+
+            //split the full name of the user to first and last name
+            string fullname = (string)userInfo.Tables[0].Rows[0]["Name"];
+            string[] name;
+
+            name = fullname.Split(' ');
+
+            string firstName = name[0];
+            string lastName = name[1];
+            string profilePic = (string)userInfo.Tables[0].Rows[0]["ProfileImg"];
+            string city = (string)userInfo.Tables[0].Rows[0]["City"];
+            string state = (string)userInfo.Tables[0].Rows[0]["State"];
+            string gender = (string)userInfo.Tables[0].Rows[0]["Gender"];
+            string height = (string)userInfo.Tables[0].Rows[0]["Height"];
+            decimal weight = (decimal)userInfo.Tables[0].Rows[0]["Weight"];
+            int age = Convert.ToInt32(userInfo.Tables[0].Rows[0]["Age"]);
+            string occupation = (string)userInfo.Tables[0].Rows[0]["Occupation"];
+            string commitment = (string)userInfo.Tables[0].Rows[0]["Commitment"];
+            string description = (string)userInfo.Tables[0].Rows[0]["Description"];
+
+            bool accVisible;
+            string visible;
+
+            if (userInfo.Tables[0].Rows[0]["Visible"] == DBNull.Value)
+            {
+                accVisible = true;
+                visible = "Visible";
+            }
+            else if (userInfo.Tables[0].Rows[0]["Visible"].Equals(true))
+            {
+                accVisible = true;
+                visible = "Visible";
+            }
+            else
+            {
+                accVisible = false;
+                visible = "Not Visible";
+            }
+            //private info 
+            string email = (string)userInfo.Tables[0].Rows[0]["Email"];
+            string address = (string)userInfo.Tables[0].Rows[0]["Address"];
+            string phoneNumber = (string)userInfo.Tables[0].Rows[0]["PhoneNumber"];
+
+            EditProfile editProfile = new EditProfile();
+            editProfile.FirstName = firstName;
+            editProfile.LastName = lastName;
+            editProfile.profilePic = profilePic;
+            editProfile.City = city;
+            editProfile.State = state;
+            editProfile.Gender = gender;
+            editProfile.Height = height;
+            editProfile.Weight = weight.ToString();
+            editProfile.Age = age;
+            editProfile.Occupation = occupation;
+            editProfile.Commitment = commitment;
+            editProfile.Description = description;
+            editProfile.Visible = visible;
+            editProfile.PhoneNumber = phoneNumber;
+            editProfile.Email = email;
+            editProfile.Address = address;
+
+            List<EditProfile> theProfile = new List<EditProfile>();
+            theProfile.Add(editProfile);
+
+            editProfile.EditProf = theProfile;
+
+            ViewBag.LoggedInUserProfile = theProfile;
+
+            return View("~/Views/Home/userProfile.cshtml", editProfile);
         }
     }
 }
--- Dating Site Razor Views/Views/Home/userProfile.cshtml.cs
+++ Dating Site Razor Views/Views/Home/userProfile.cshtml.cs
@@ -13,5 +13,30 @@ using System.Data;
 
 namespace Dating_Site_Razor_Views.Pages
 {
+    public class UserProfile : PageModel
+    {
 
+    }
+    public class EditProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Gender { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+        public string Commitment { get; set; }
+        public string Description { get; set; }
+        public string profilePic { get; set; }
+
+        public string Visible { get; set; }
+
+        public List<EditProfile> EditProf { get; set; }
+    }
 }
