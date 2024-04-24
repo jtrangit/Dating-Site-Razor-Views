1e25371fd27f898491ac826e3cf56692065983ac
Johnny Tran <jojox210@gmail.com>
2024-04-24 13:08:55 -0400

    created view for showing likes.

---
+++ Dating Site Razor Views/Controllers/LikesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dating_Site_Razor_Views.Controllers
+{
+    public class LikesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Likes()
+        {
+            return View("~/Views/Likes/Likes.cshtml");
+        }
+    }
+}
