c4a58ef52a76e82ab7d9fd34ea11302ff9fd8eaa
Johnny Tran <jojox210@gmail.com>
2024-04-25 23:45:26 -0400

    3rd party api

--- Love Calculator API/Controllers/CalculatorController.cs
+++ Love Calculator API/Controllers/CalculatorController.cs
@@ -34,6 +34,9 @@ namespace Love_Calculator_API.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            //API KEY: "X-RapidAPI-Key", "1aaea12668msh25d702e80d1c95cp14501fjsn28916c99fb9c"
+            //API HOST "X-RapidAPI-Host", "love-calculator.p.rapidapi.com"
+            //URL "https://love-calculator.p.rapidapi.com/getPercentage"
             var client = new RestClient($"https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John");
             var request = new RestRequest(Method.GET);
             
